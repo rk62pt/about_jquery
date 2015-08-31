3c20dbec75c5caecfd508a62a605b69b83f776b1
rk62pt <rk62pt@gmail.com>
2015-08-31 23:43:09 +0800

    jquery popup
    + jquery animate

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,5 +1,5 @@
 $.fn.extend({
-	popup:function(option){
+	popup:function(openOrClose,option){
 		var _clientWidth = document.body.clientWidth;
 		//var _clientHeight = document.body.clientHeight;
 		var _clientHeight = window.screen.availHeight;
@@ -10,13 +10,15 @@ $.fn.extend({
 		console.log(_clientHeight);
 		console.log(_height);
 		console.log((_clientHeight-_height)/2);
+		var _mask = $("<div></div>").attr("id",_id+"_mask").addClass("pop-mask");
 		var _body = $("<div></div>")
 			   .attr("id",_id+"_pop_body")
 			   .css("width",_width+"px")
 			   .css("height",_height+"px")
 			   .css("left",(_clientWidth-_width)/2+"px")
-			   .css("top",(_clientHeight-_height)/3+"px")
-			   .addClass("pop-body");
+			   //.css("top",(_clientHeight-_height)/3+"px")
+			   .css("top","-"+_height+"px")
+				 .addClass("pop-body");
 		var _header = $("<div></div>")
 		   .attr("id",_id+"_pop_header")
 		   .css("height","30px")
@@ -31,9 +33,25 @@ $.fn.extend({
 					   .html("hello!!");
 		_body.append(_header)
 			 .append(_content);
-		this.append(_body);
-		console.log(this);
-		this.addClass("pop-mask");
+		_mask.append(_body);
+		//this.addClass("pop-mask");
+		if("open"===openOrClose){
+			this.after(_mask);
+			var _sliderIn = (_clientHeight-_height)/3+"px";
+			$("#"+_id+"_pop_body").animate({top:_sliderIn},400);//.addClass("open");
+		}else if("close"===openOrClose){
+			var _sliderOut = "-"+_height+"px";
+			$("#"+_id+"_pop_body").animate({top:_sliderOut},400,function(){
+				$("#"+_id+"_mask").remove();
+			});
+		}
+		_mask.on("click",function(){
+			var _sliderOut = "-"+_height+"px";
+			$("#"+_id+"_pop_body").animate({top:_sliderOut},400,function(){
+				$("#"+_id+"_mask").remove();
+			});
+			//$("#"+_id+"_mask").remove();
+		});
 		return this;
 	}
 });
